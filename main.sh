4560b07d55bf392d84924fc4a10a568081adcbc6
snowsphere <info@snowsphere.net>
2025-01-09 23:59:32 +0900

    feat: homebrewインストール後のコマンドを追加

--- main.sh
+++ main.sh
@@ -5,6 +5,7 @@ mkdir Workspaces
 
 xcode-select --install
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 
 cp ./dotfiles/.Brewfile ~/.Brewfile
 
